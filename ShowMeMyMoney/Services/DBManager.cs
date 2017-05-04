4ab3c4e67ed2032dcd0f18b0758eacafd1d36e8b
mukae1997 <444652692@qq.com>
2017-05-04 23:12:17 +0800

    Add files via upload

---
+++ ShowMeMyMoney/Services/DBManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowMeMyMoney.Services
+{
+    public class DBManager
+    {
+        public DBManager()
+        {
+            LoadDatabase();
+        }
+
+        private void LoadDatabase()
+        {
+            // Get a reference to the SQLite database
+            /*
+             Practically this TABLE is UNCOMPLETED as
+             this db does not include the field of 'completed'.
+             Technically, it should be included.
+             
+             */
+            string sql = @"CREATE TABLE IF NOT EXISTS
+                          accounts  (Id      VARCHAR(30) PRIMARY KEY NOT NULL,
+                                    description    VARCHAR( 400 ),
+                                    createDate VARCHAR( 30 ),
+                    );";
+            /* SQL  not complete yet */
+            using (var statement = App.conn.Prepare(sql))
+            {
+                statement.Step();
+            }
+        }
+    }
+}
