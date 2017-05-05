357953e379bb197936b34d38c72c18dc854dd21a
mukae1997 <444652692@qq.com>
2017-05-05 16:04:19 +0800

    Add files via upload

---
+++ Model/categoryItem.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowMeMyMoney.Model
+{
+     public class categoryItem
+    {
+        public string name;
+        public int number;
+        public string color;
+        public double share;/* 分类当前所占百分比 */
+        [JsonConstructor]
+        public categoryItem(int i, string s,  double _share, string c)
+        {
+            name = s;
+            number = i;
+            color = c;
+            share = _share;
+        }
+        public categoryItem(string s, int i, string c)
+        {
+            name = s;
+            number = i;
+            color = c; 
+        }
+    }
+}
