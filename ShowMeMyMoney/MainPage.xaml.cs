c9c771eae73ea52472935fde103b5fcfa07b485c
mukae1997 <444652692@qq.com>
2017-05-06 14:36:09 +0800

    Add files via upload

--- ShowMeMyMoney/MainPage.xaml.cs
+++ ShowMeMyMoney/MainPage.xaml.cs
@@ -5,9 +5,11 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -15,6 +17,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 //“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
 
@@ -28,20 +31,36 @@ namespace ShowMeMyMoney
         public MainPage()
         {
             this.InitializeComponent();
-            initializeCategoryTable();
+            remainedProportion = 100;
+            //    initializeCategoryTable(); 
+
+            /* reset, for debugging  */
+            categoryTable = new ObservableCollection<categoryItem>();
+            categoryTable.Clear();
+            categoryTable.Add(new categoryItem(-1, "Total", 100, "Black"));
+        //    saveCategoryTable();
         }
-        private void setProPortionBar()
+        private void addShareBar(categoryItem c)
         {
-            /* 从categoryTable里读取分类的比例和颜色，并设置到 “proportionBar”的各个长方形中  */
+            /* 从categoryItem里读取分类的比例和颜色，并设置到 “ShareBar”的各个长方形中  */
+            
+                if (c.number == -1) return;
+                Rectangle r = new Rectangle();
+                r.Fill = new SolidColorBrush((Color)typeof(Colors).GetProperty(c.color).GetValue(this)); 
+                r.Width = c.share / 100 * 400;
+                shareBar.Children.Insert(0, r);
+            
+
         }
         private ObservableCollection<categoryItem> categoryTable;
         private int categoryCount;
+        private double remainedProportion;
 
         private void ShowCategory_Click(object sender, ItemClickEventArgs e)
         {
-            /* 将分类编号发送到accountsListViewPage */
-            int Category_number = ((categoryItem)(e.ClickedItem)).number;
-            Frame.Navigate(typeof(NewPage), Category_number);
+            /* 将分类item发送到accountsListViewPage */
+            categoryItem citem = ((categoryItem)(e.ClickedItem));
+            Frame.Navigate(typeof(AccountsListViewPage), citem);
         }
 
 
@@ -58,6 +77,12 @@ namespace ShowMeMyMoney
                     /* 第一次打开应用, 则无需进行后续操作 */
                     return;
                 }
+
+
+                /* 手动添加一个代表总开支的item */
+                categoryTable.Add(new categoryItem(-1, "Total", 100, "Black"));
+
+
                 var file = await Folder.GetFileAsync("categoryTable.json");
                 
                 var data = await file.OpenReadAsync();
@@ -65,13 +90,23 @@ namespace ShowMeMyMoney
                 using (StreamReader r = new StreamReader(data.AsStream()))
                 {
                     string text = r.ReadToEnd();
+
+                    /* 如果json是空的，也不能进行后续操作*/
+                    if (text == "") return;
                     /* 将json文件中的东西反序列化，加入到catagoryItem的数组   */
                     categoryItem[] p = JsonConvert.DeserializeObject<categoryItem[]>(text);
                     foreach (var i in p)
                     {
                         categoryTable.Add(i);
+                        /* 不断调整remainedProportion */
+                        remainedProportion -= i.share;
                         categoryCount++;
                     }
+                }
+
+                foreach(var c in categoryTable)
+                {
+                    addShareBar(c);
                 }
                 
             }
@@ -104,19 +139,35 @@ namespace ShowMeMyMoney
 
         private async void AddNewCategoryButton_Click(object sender, RoutedEventArgs e)
         {
-            ContentDialog dialog = AddNewCategoryDialog; 
+            ContentDialog dialog = AddNewCategoryDialog;
+            categoryShare.Header = "# 所占预算比例（百分比数值，1-100），还剩"+remainedProportion+"%可用";
             await dialog.ShowAsync();
         }
 
-        private void AddNewCategoryDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        private async void AddNewCategoryDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
-
+            if (remainedProportion < Convert.ToDouble(categoryShare.Text)) {
+                /* 判断新添加的分类比例是否合法 */
+                AddNewCategoryDialog.Hide();
+                ContentDialog alert = new ContentDialog() {
+                    Title = "错误提示",
+                    Content = "新设置的比例太大啦！只剩" + remainedProportion + "%可用。",
+                    IsPrimaryButtonEnabled = true,
+                   PrimaryButtonText = "OK"
+                    
+                };
+                await alert.ShowAsync();
+                return;
+            }
             /* 添加新分类 */
             categoryItem newCategory = new categoryItem(categoryCount, categoryName.Text, Convert.ToDouble(categoryShare.Text), categoryColor.Text);
             categoryTable.Add(newCategory);
             categoryCount++;
+            remainedProportion -= newCategory.share;
             saveCategoryTable();
             AddNewCategoryDialog.Hide();
+            /* 添加shareBar的条条 */
+            addShareBar(newCategory);
 
         }
 
