bf0c8f3447562ebd72123fd62a612079c846474a
mukae1997 <444652692@qq.com>
2017-05-09 18:11:45 +0800

    修改MainPage和Account
    MainPage更新：
    - 重复约束：禁止类名重复
    - 添加ColorPicker
    - 只发送categoryTable给Account；
    - 稍微美化
    Account更新：
    - 无须发送ViewModel给MainPage

---
+++ ShowMeMyMoney/Account.xaml.cs
@@ -0,0 +1,99 @@
+﻿using ShowMeMyMoney.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace ShowMeMyMoney
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class Account : Page
+    {
+        private ViewModel.ViewModel ViewModel;
+        public Account()
+        {
+
+            ViewModel = new ViewModel.ViewModel();
+            this.InitializeComponent();
+        }
+        //  待考虑，是否可以修改
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e.Parameter.GetType() == typeof(ObservableCollection<categoryItem>))
+            {
+                ViewModel.AllCatagoryItem = (ObservableCollection<categoryItem>)(e.Parameter);
+
+                //  去掉 Total 这一分类
+                ViewModel.AllCatagoryItem.RemoveAt(0);
+            }
+        }
+        private bool checkInput()
+        {
+            string warning = "";
+            if (Category.SelectedIndex == -1)
+            {
+                warning += "请选择类别\n";
+            }
+            if (!Regex.IsMatch(Amount.Text, @"^(-?\d+)(\.\d+)?$"))
+            {
+                warning += "金额输入有误\n";
+            }
+            //  bool isPocketMoney = (category == "私房钱") ? true : false;
+            //   bool inOrOut = (bool)income.IsChecked;
+            if (Description.Text == "")
+            {
+                warning += "请输入描述\n";
+            }
+            if (warning != "")
+            {
+                var i = new MessageDialog(warning).ShowAsync();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private void createButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!checkInput()) return;
+            string category = Category.SelectedItem.ToString();
+            DateTimeOffset date = Date.Date;
+
+            double amount = Convert.ToDouble(Amount.Text);
+            bool isPocketMoney = (category == "私房钱") ? true : false;
+            bool inOrOut = (bool)income.IsChecked;
+            string description = Description.Text;
+            ViewModel.AddAccountItem(category, date, amount, isPocketMoney, inOrOut, description);
+            Frame.Navigate(typeof(MainPage), ViewModel);
+        }
+
+        private void canclButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Category.SelectedIndex = 0;
+            Date.Date = DateTime.Now;
+            Amount.Text = "";
+            income.IsChecked = false;
+            Description.Text = "";
+            Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
--- ShowMeMyMoney/MainPage.xaml.cs
+++ ShowMeMyMoney/MainPage.xaml.cs
@@ -31,31 +31,51 @@ namespace ShowMeMyMoney
         public MainPage()
         {
             this.InitializeComponent();
+            this.ViewModel = new ShowMeMyMoney.ViewModel.ViewModel();
             remainedProportion = 100;
-            //    initializeCategoryTable(); 
+                initializeCategoryTable();
 
-            /* reset, for debugging  */
+            /* reset, for debugging  
             categoryTable = new ObservableCollection<categoryItem>();
             categoryTable.Clear();
             categoryTable.Add(new categoryItem(-1, "Total", 100, "Black"));
-        //    saveCategoryTable();
+            //    saveCategoryTable();*/
+
+            makeColorPicker();
+            initializeShareSlider();
         }
+
+      
+
         private void addShareBar(categoryItem c)
         {
             /* 从categoryItem里读取分类的比例和颜色，并设置到 “ShareBar”的各个长方形中  */
-            
-                if (c.number == -1) return;
-                Rectangle r = new Rectangle();
-                r.Fill = new SolidColorBrush((Color)typeof(Colors).GetProperty(c.color).GetValue(this)); 
-                r.Width = c.share / 100 * 400;
-                shareBar.Children.Insert(0, r);
-            
+
+            if (c.number == -1) return;
+            Rectangle r = new Rectangle();
+            r.Fill = new SolidColorBrush((Color)typeof(Colors).GetProperty(c.color).GetValue(this));
+            r.Width = c.share / 100 * 400;
+            shareBar.Children.Insert(0, r);
+
 
         }
         private ObservableCollection<categoryItem> categoryTable;
         private int categoryCount;
         private double remainedProportion;
+        ViewModel.ViewModel ViewModel { get; set; }
 
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e.Parameter != null)
+            {
+
+                if (e.Parameter.GetType() == typeof(ViewModel.ViewModel))
+                {
+                    this.ViewModel = ((ViewModel.ViewModel)e.Parameter);
+                }
+                ViewModel.SelectedItem = null;
+            }
+        }
         private void ShowCategory_Click(object sender, ItemClickEventArgs e)
         {
             /* 将分类item发送到accountsListViewPage */
@@ -64,27 +84,28 @@ namespace ShowMeMyMoney
         }
 
 
-        private async void initializeCategoryTable() {
+        private async void initializeCategoryTable()
+        {
             categoryTable = new ObservableCollection<categoryItem>();
             categoryCount = 0;
             try
-            { 
+            {
                 /*  读取json文件  */
                 var Folder = Windows.Storage.ApplicationData.Current.LocalFolder;
                 var item = await Folder.TryGetItemAsync("categoryTable.json");
                 if (item == null)
                 {
                     /* 第一次打开应用, 则无需进行后续操作 */
+                    /* 手动添加一个代表总开支的item */
+                    categoryTable.Add(new categoryItem(-1, "Total", 100, "Black"));
                     return;
                 }
 
 
-                /* 手动添加一个代表总开支的item */
-                categoryTable.Add(new categoryItem(-1, "Total", 100, "Black"));
 
 
                 var file = await Folder.GetFileAsync("categoryTable.json");
-                
+
                 var data = await file.OpenReadAsync();
 
                 using (StreamReader r = new StreamReader(data.AsStream()))
@@ -99,16 +120,17 @@ namespace ShowMeMyMoney
                     {
                         categoryTable.Add(i);
                         /* 不断调整remainedProportion */
+                        if (i.number != -1)
                         remainedProportion -= i.share;
                         categoryCount++;
                     }
                 }
 
-                foreach(var c in categoryTable)
+                foreach (var c in categoryTable)
                 {
                     addShareBar(c);
                 }
-                
+
             }
             catch (Exception e)
             {
@@ -116,7 +138,8 @@ namespace ShowMeMyMoney
             }
         }
 
-        private async void saveCategoryTable() {
+        private async void saveCategoryTable()
+        {
             try
             {
                 var Folder = Windows.Storage.ApplicationData.Current.LocalFolder;
@@ -140,35 +163,65 @@ namespace ShowMeMyMoney
         private async void AddNewCategoryButton_Click(object sender, RoutedEventArgs e)
         {
             ContentDialog dialog = AddNewCategoryDialog;
-            categoryShare.Header = "# 所占预算比例（百分比数值，1-100），还剩"+remainedProportion+"%可用";
+            shareSlider.Header = "# 所占预算比例，还剩" + remainedProportion + "%可用";
             await dialog.ShowAsync();
         }
 
         private async void AddNewCategoryDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
-            if (remainedProportion < Convert.ToDouble(categoryShare.Text)) {
-                /* 判断新添加的分类比例是否合法 */
+            /*
+            if (remainedProportion < Convert.ToDouble(categoryShare.Text))
+            {
+               //  /* 判断新添加的分类比例是否合法 
                 AddNewCategoryDialog.Hide();
-                ContentDialog alert = new ContentDialog() {
+                ContentDialog alert = new ContentDialog()
+                {
                     Title = "错误提示",
                     Content = "新设置的比例太大啦！只剩" + remainedProportion + "%可用。",
                     IsPrimaryButtonEnabled = true,
-                   PrimaryButtonText = "OK"
-                    
+                    PrimaryButtonText = "OK"
+
                 };
                 await alert.ShowAsync();
                 return;
+            } 
+        */
+
+
+            categoryItem newCategory = new categoryItem(categoryCount, categoryName.Text, shareSlider.Value, categoryColor.Text);
+
+             /* 分类名称重复约束 */
+            foreach (var item in categoryTable)
+            {
+                if (item.name == newCategory.name) {
+                    AddNewCategoryDialog.Hide();
+                    ContentDialog alert = new ContentDialog()
+                    {
+                        Title = "错误提示",
+                        Content = "分类名字重复！换个名字吧~",
+                        IsPrimaryButtonEnabled = true,
+                        PrimaryButtonText = "OK"
+
+                    };
+                    await alert.ShowAsync();
+                    return;
+                }
+                    
             }
+
             /* 添加新分类 */
-            categoryItem newCategory = new categoryItem(categoryCount, categoryName.Text, Convert.ToDouble(categoryShare.Text), categoryColor.Text);
             categoryTable.Add(newCategory);
             categoryCount++;
+            
             remainedProportion -= newCategory.share;
             saveCategoryTable();
             AddNewCategoryDialog.Hide();
             /* 添加shareBar的条条 */
             addShareBar(newCategory);
+            /* 修改滑块的上限 */
 
+            shareSlider.Maximum = remainedProportion;
+            //  ViewModel.AddCategoryItemm(newCategory);
         }
 
         private void AddNewCategoryDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
@@ -179,7 +232,41 @@ namespace ShowMeMyMoney
         private void AddNewAccountButton_Click(object sender, RoutedEventArgs e)
         {
 
-            Frame.Navigate(typeof(NewPage));
+            Frame.Navigate(typeof(Account), categoryTable);
+        }
+        private void makeColorPicker()
+        {
+            var colors = typeof(Colors).GetTypeInfo().DeclaredProperties;
+
+           
+
+            foreach (var item in colors)
+            {
+                /* TODO: 删除已经被选走的颜色 。 要实现该功能，还要在新增分类时维护颜色表。  */
+                ColorPallete_Combo.Items.Add(item);
+            }
+
+            
+        }
+        private void ColorPalleteCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ColorPallete_Combo.SelectedIndex != -1)
+            {
+                var pi = ColorPallete_Combo.SelectedItem as PropertyInfo;
+                ColorPallete_Combo.BorderBrush = new SolidColorBrush((Color)pi.GetValue(null));
+                var colorName = pi.Name;
+                categoryColor.Text = colorName;
+            }
+        }
+
+        private void shareSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+           /* 好像不需要干嘛 */
+        }
+
+        private void initializeShareSlider()
+        {
+            shareSlider.Maximum = remainedProportion;
         }
     }
 
